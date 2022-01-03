e1d6e8f98af6084848a81f55c7b7ebc2dd24551f
ChuckWagon2480 <78745580+ChuckWagon2480@users.noreply.github.com>
2022-01-04 08:41:46 +0900

    Revert "Feature/post"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { SpaceModule } from './space/space.module';
-import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'classum1234',
-      database: 'classum',
-      entities: ['dist/**/*.entity.{ts,js}'],
-      synchronize: true,
-    }),
-    UserModule,
-    AuthModule,
-    SpaceModule,
-    PostModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/space/dto/create-space.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class CreateSpaceDto {
-  @IsString()
-  @IsNotEmpty()
-  readonly name: string;
-}
