30b1d6c3aa6bdfb58f75828c1ffecaadf77b2e93
ChuckWagon2480 <ghwar100@gmail.com>
2022-01-04 23:17:43 +0900

    [FEAT] Chat Update, Delete, Restore API

--- src/chat/dto/update-chat.dto.ts
+++ src/chat/dto/update-chat.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateChatDto } from './create-chat.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateChatDto extends PartialType(CreateChatDto) {}
+export class UpdateChatDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly content: string;
+}
